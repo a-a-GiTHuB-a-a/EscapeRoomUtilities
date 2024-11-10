96cd19e983474df937837e2f5fe89ea5da918154
a-a-GiTHuB-a-a <64599476+a-a-GiTHuB-a-a@users.noreply.github.com>
2024-11-10 12:50:22 -0500

    remove some uselessness

--- src/main/java/com/daboxen/EscapeRoomUtilities/EscapeRoomUtilities.java
+++ src/main/java/com/daboxen/EscapeRoomUtilities/EscapeRoomUtilities.java
@@ -3,14 +3,11 @@ package com.daboxen.EscapeRoomUtilities;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
-import com.mojang.serialization.Codec;
 
 import net.minecraft.client.Minecraft;
-import net.minecraft.core.component.DataComponentType;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
-import net.minecraft.network.codec.ByteBufCodecs;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
@@ -59,17 +56,6 @@ public class EscapeRoomUtilities
 
         public static final DeferredItem<Item> HEAL_ORB = ITEMS.registerSimpleItem("heal_orb", new Item.Properties().fireResistant().food(new FoodProperties.Builder()
                 .alwaysEdible().nutrition(20).saturationModifier(.5f).fast().usingConvertsTo(() -> EscapeRoomUtilities.HEAL_ORB.get()).build()));
-
-        public static final DeferredRegister.DataComponents DATA_COMPONENTS = DeferredRegister.createDataComponents(MODID);
-
-        public static final DeferredHolder<DataComponentType<?>, DataComponentType<Integer>> TIME_TO_LIVE = DATA_COMPONENTS.registerComponentType(
-                "time_to_live",
-                builder -> builder
-                        // The codec to read/write the data to disk
-                        .persistent(Codec.INT)
-                        // The codec to read/write the data across the network
-                        .networkSynchronized(ByteBufCodecs.INT)
-        );
 
         // Creates a creative tab with the id "examplemod:example_tab" for the example item, that is placed after the combat tab
         public static final DeferredHolder<CreativeModeTab, CreativeModeTab> EXAMPLE_TAB = CREATIVE_MODE_TABS.register("escape_room_items", () -> CreativeModeTab.builder()
